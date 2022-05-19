4520901ad91746949ebcc87a05c7c61342cc2348
dksath <72483382+dksath@users.noreply.github.com>
2022-05-19 12:12:22 +0800

    second push

--- templates/static/src/App.js
+++ templates/static/src/App.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App</h1>
-            </div>
-        )
-    }
+function Header() {
+  return (
+    <div>
+      <header className="Header">
+        <p>
+          S21 Gait Recognition
+        </p>
+      </header>
+    </div>
+  );
 }
 
-export default App
+function Body() {
+  return (
+    <div className='Body'> 
+      <p>
+        Test
+      </p>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        className="Link-light"
+        href="https://github.com/dksath/gait_recognition"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gait Recognition Repository
+      </a>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
